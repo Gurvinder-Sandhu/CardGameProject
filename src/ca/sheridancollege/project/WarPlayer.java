76b626b931344f8f8f3d832f9a706c2c7a811bf1
Gurvinderbeer Singh <nagurvi@sheridancollege.ca>
2024-11-27 16:35:05 -0500

    Update WarPlayer.java

--- src/ca/sheridancollege/project/WarPlayer.java
+++ src/ca/sheridancollege/project/WarPlayer.java
@@ -10,46 +10,43 @@ package ca.sheridancollege.project;
  * @author Harnoor Singh
  */
 
-import java.util.ArrayList;
+
+import java.util.LinkedList;
 
 public class WarPlayer extends Player {
-    private ArrayList<Card> hand; // Cards in the player's hand
+    private LinkedList<Card> hand;
 
     public WarPlayer(String name) {
         super(name);
-        hand = new ArrayList<>(); // Initialize an empty hand
+        hand = new LinkedList<>();
+    }
+
+    public void receiveCard(Card card) {
+        if (card != null) {
+            hand.addLast(card);
+        }
+    }
+
+    public void receiveCard(Card[] cards) {
+        for (Card card : cards) {
+            receiveCard(card);
+        }
+    }
+
+    public Card playCard() {
+        return hand.isEmpty() ? null : hand.removeFirst();
+    }
+
+    public int getHandSize() {
+        return hand.size();
+    }
+
+    public boolean hasCards() {
+        return !hand.isEmpty();
     }
 
     @Override
     public void play() {
-        System.out.println(getPlayerName() + " plays a card.");
-    }
-
-    public Card playCard() {
-        if (hand.isEmpty()) {
-            System.out.println(getPlayerName() + " has no more cards.");
-            return null; // No cards left to play
-        }
-        return hand.remove(0); // Removes and returns the top card from hand
-    }
-
-    public void receiveCard(Card card) {
-        hand.add(card); // Add a single card to the player's hand
-    }
-
-    public boolean hasCards() {
-        return !hand.isEmpty(); // Check if the player has cards
-    }
-
-    public int getHandSize() {
-        return hand.size(); // Get the current hand size
-    }
-
-    public ArrayList<Card> getCards() {
-        return hand; // Get the list of cards in hand
-    }
-
-    public void receiveCards(ArrayList<Card> cards) {
-        hand.addAll(cards); // Add multiple cards to the player's hand
+        System.out.println(getPlayerName() + " is playing.");
     }
 }
